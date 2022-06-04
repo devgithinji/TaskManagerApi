03900270de8cbd5cfa21106090e3964cd9b93461
dennis <wakahiad@gmail.com>
2022-06-04 17:16:09 +0300

    controller methods impl

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,66 @@
-const getAllTasks = (req, res) => {
-    res.send('all items from the file')
+const Task = require('../models/task')
+
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json({tasks})
+    } catch (e) {
+        res.status(500).json({msg: e})
+    }
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json(task)
+    } catch (e) {
+        res.status(500).json({msg: e})
+    }
+
 }
 
-const getTask = (req, res) => {
-    res.send('get single task')
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOne({_id: taskID});
+
+        if (!task) {
+            return res.status(404).json({message: `no task found with id:  ${taskID}`})
+        }
+
+        res.status(200).json(task)
+    } catch (e) {
+        res.status(500).json({msg: e})
+    }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task')
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).json({message: `no task found with id:  ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (e) {
+        res.status(500).json({msg: e})
+    }
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id: taskID})
+        if (!task) {
+            return res.status(404).json({message: `no task found with id:  ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (e) {
+        res.status(500).json({msg: e})
+    }
 }
 
 module.exports = {
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
-const connectionString = 'mongodb+srv://taskmanager:taskmanager1234@cluster0.lorgx6p.mongodb.net/taskmanager?retryWrites=true&w=majority';
 
-const connectDB = () => {
-    return mongoose.connect(connectionString, {
+const connectDB = (url) => {
+    return mongoose.connect(url, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
